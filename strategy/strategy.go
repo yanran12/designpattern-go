c81b03f633c50949dfbb5cc47f04efbb0b692dd7
yanran12 <yanran12@126.com>
2021-04-06 11:26:24 +0800

    refactor: 优化策略模式实现

--- strategy/strategy.go
+++ strategy/strategy.go
@@ -2,31 +2,41 @@ package strategy
 
 import "fmt"
 
-type Decompression interface {
+type DecompressionStrategy interface {
 	Decompress(file string)
 }
 
-type RarDecompression struct {
+type RarDecompressionStrategy struct {
 }
 
-func (rd *RarDecompression) Decompress(file string) {
+func (rd *RarDecompressionStrategy) Decompress(file string) {
 	fmt.Println("用rar的方式解压", file)
 }
 
-type TarGzDecompression struct {
+type TarGzDecompressionStrategy struct {
 }
 
-func (td *TarGzDecompression) Decompress(file string) {
+func (td *TarGzDecompressionStrategy) Decompress(file string) {
 	fmt.Println("用tar.gz的方式解压", file)
+}
+
+type Decompression struct {
+	decompressionStrategy DecompressionStrategy
+}
+
+func NewDecompression(decompressionStrategy DecompressionStrategy) *Decompression {
+	return &Decompression{decompressionStrategy}
+}
+
+func (d *Decompression) Decompression(file string) {
+	d.decompressionStrategy.Decompress(file)
 }
 
 func Run() {
 	file := "要解压的文件"
 
-	rd := &RarDecompression{}
-	rd.Decompress(file)
+	NewDecompression(&RarDecompressionStrategy{}).Decompression(file)
 
-	td := &TarGzDecompression{}
-	td.Decompress(file)
+	NewDecompression(&TarGzDecompressionStrategy{}).Decompression(file)
 
 }
